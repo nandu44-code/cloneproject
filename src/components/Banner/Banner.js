89f56ef70e07d8fdd10a44ef5228198ccbaccad6
nandu44-code <nanduganeshnair4444@gmail.com>
2023-12-26 20:04:29 +0530

    Added Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons' >
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>lorem and every tig in the world ois quite powerrful and we can achieve all those thnga with very fascinating way and it is neithter be created not be destroyed and it can mbaoidsfisd fhwdiho ihdofj  </h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Banner
